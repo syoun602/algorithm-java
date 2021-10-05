f1059abb6ab122e3fb3bd4ece8a2e6c8adb13663
Sun <syoun602@gmail.com>
2021-10-05 21:50:21 +0900

    10/05/2021

---
+++ src/baekjoon/problem15552/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.problem15552;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(in.readLine());
+        int a, b = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(in.readLine(), " ");
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
