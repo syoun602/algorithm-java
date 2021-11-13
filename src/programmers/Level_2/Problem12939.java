75a44783e529947c26c44ecf3c398abbc381a175
Sun <syoun602@gmail.com>
2021-11-13 21:59:23 +0900

    11/13/2021

---
+++ src/programmers/Level_2/Problem12939.java
@@ -0,0 +1,34 @@
+package programmers.Level_2;
+
+import java.util.StringTokenizer;
+
+/*
+Problem 12939 - 최댓값과 최솟값 (Level 2)
+Link to Problem - https://programmers.co.kr/learn/courses/30/lessons/12939
+*/
+
+public class Problem12939 {
+
+    public String solution(String s) {
+        String answer = "";
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(s);
+        int min, max, n;
+
+        min = max = Integer.parseInt(st.nextToken());
+
+        while(st.hasMoreTokens()) {
+            n = Integer.parseInt(st.nextToken());
+
+            if(min > n)
+                min = n;
+            if(max < n)
+                max = n;
+        }
+
+        sb.append(min).append(" ").append(max);
+
+        return sb.toString();
+    }
+}
